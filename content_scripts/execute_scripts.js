6c04c895db1f863a781a4707e72db50dfb61fb7d
MadsLang <madslangs@gmail.com>
2022-11-02 15:53:38 +0100

    init

---
+++ content_scripts/execute_scripts.js
@@ -0,0 +1,49 @@
+(() => {
+    /**
+     * Check and set a global guard variable.
+     * If this content script is injected into the same page again,
+     * it will do nothing next time.
+     */
+    if (window.hasRun) {
+      return;
+    }
+    window.hasRun = true;
+  
+    /**
+     * Function 1: Justify all paragraphs. 
+     */
+    function doStuff() {
+      document.querySelectorAll('[role=paragraph],p').forEach(function(node) {
+        node.style.textAlign = "justify"
+    });
+    }
+
+    /**
+     * Add functions similar to doStuff here for more functions
+     */
+  
+    /**
+     * Does not do anything. Refresh the page instead to rm edits
+     */
+    function removeStuff() {
+
+    }
+  
+    /**
+     * Listen for messages from the background script.
+     * If receiving "justify", it calls doStuff function.
+     * Add other messages and corresponding function to add more functions
+     */
+    browser.runtime.onMessage.addListener((message) => {
+      if (message.command === "justify") {
+        doStuff();
+      } else if (message.command === "reset") {
+        removeStuff();
+      }
+    });
+  })();
+  
+
+
+
+
---
+++ popup/popup.js
@@ -0,0 +1,76 @@
+
+/**
+* Listen for clicks on the buttons, and send the appropriate message to
+* the content script in the page.
+*/
+function listenForClicks() {
+    document.addEventListener("click", (e) => {
+
+
+    /**
+    * Insert the page-hiding CSS into the active tab,
+    * then get the beast URL and
+    * send a "justify" message to the content script in the active tab.
+    */
+    function justify(tabs) {
+    browser.tabs.sendMessage(tabs[0].id, {
+    command: "justify"
+    });
+    }
+
+    /** 
+     * send a "reset" message to the content script in the active tab.
+     * But is does not do anything right now
+    */
+    function reset(tabs) {
+    browser.tabs.sendMessage(tabs[0].id, {
+    command: "reset",
+    });
+    }
+
+    /**
+    * Just log the error to the console.
+    */
+    function reportError(error) {
+    console.error(`Could not justify: ${error}`);
+    }
+
+    /**
+    * Get the active tab,
+    * then call "justify()" or "reset()" as appropriate.
+    * Add more messages in the if-else for more functions
+    */
+    if (e.target.classList.contains("justify")) {
+    browser.tabs
+    .query({ active: true, currentWindow: true })
+    .then(justify)
+    .catch(reportError);
+    } else if (e.target.classList.contains("reset")) {
+    browser.tabs
+    .query({ active: true, currentWindow: true })
+    .then(reset)
+    .catch(reportError);
+    }
+    });
+}
+
+/**
+* There was an error executing the script.
+* Display the popup's error message, and hide the normal UI.
+*/
+function reportExecuteScriptError(error) {
+document.querySelector("#popup-content").classList.add("hidden");
+document.querySelector("#error-content").classList.remove("hidden");
+console.error(`Failed to execute content script: ${error.message}`);
+}
+
+/**
+* When the popup loads, inject a content script into the active tab,
+* and add a click handler.
+* If we couldn't inject the script, handle the error.
+*/
+browser.tabs
+.executeScript({ file: "/content_scripts/execute_scripts.js" })
+.then(listenForClicks)
+.catch(reportExecuteScriptError);
+
